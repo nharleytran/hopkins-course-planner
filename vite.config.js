4d7fe4817d2cc5de5474c49eed17cd3bc8b181f9
nharleytran <98291123+nharleytran@users.noreply.github.com>
2022-10-30 21:23:30 -0400

    Everything

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/", // TODO Update this
+  base: "/homework-5-nharleytran", // TODO Update this
   plugins: [react()]
 })
